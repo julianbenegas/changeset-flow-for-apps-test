0e8a63330d3bf13b56c2cc530e64fb557fb707d3
Julian Benegas <julianbenegas99@gmail.com>
2023-09-07 10:28:09 -0300

    fix?

--- actions/tsup.config.ts
+++ actions/tsup.config.ts
@@ -1,18 +1,42 @@
 import { defineConfig } from "tsup";
 import fs from "fs";
+import { globby } from "globby";
+import path from "path";
 
-export default defineConfig((options) => {
-  // get all files from actions folder
-  const files = fs
-    .readdirSync("./actions")
-    .filter((file) => file.endsWith(".ts") && file !== "tsup.config.ts");
+export default defineConfig(async (options) => {
+  const actions = (await globby(["actions/*/*.ts"])).map((path) => {
+    const splitted = path.split("/");
+    const actionPath =
+      splitted.slice(0, splitted.length - 1).join("/") + "/action.yml";
+    return {
+      entry: path,
+      actionPath,
+    };
+  });
+
+  const entry: Record<string, string> = {};
+  actions.forEach((a) => {
+    const splitted = a.entry.split("/");
+    const entryName = splitted[splitted.length - 2];
+    if (!entryName) throw new Error("No file name");
+    entry[entryName + "/index"] = a.entry;
+  });
 
   return {
     minify: !options.watch,
     dts: false,
-    entry: files.map((f) => `actions/${f}`),
+    entry,
+    outDir: "actions/dist",
     format: ["cjs"],
-    outDir: "./actions/dist",
     target: "node18",
+    async onSuccess() {
+      // copy action.yml files into dist
+      actions.forEach((a) => {
+        const fullPath = path.join(process.cwd(), a.actionPath);
+        const newPath = fullPath.replace("/actions/", "/actions/dist/");
+        fs.copyFileSync(fullPath, newPath);
+      });
+      return;
+    },
   };
 });
